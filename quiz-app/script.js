01016a2d30196760a899d4c9c77719de058eb033
Pande Yogam <pandeyogam321@gmail.com>
2023-09-18 10:08:22 +0700

    modified:   base-project/style.css 	new file:   quiz-app/index.html 	new file:   quiz-app/script.js 	new file:   quiz-app/style.css

---
+++ quiz-app/script.js
@@ -0,0 +1,29 @@
+const = quizData [
+    {
+        question: 'Tanggal jadian ?',
+        a: '27',
+        b: '28',
+        c: '29',
+        d: '30',
+        correct: 'b'
+    },{
+        question: 'Nama Lengkap Pacar',
+        a: 'Pande Putu Sunaryang',
+        b: 'Pande Pt Sunariyang',
+        c: 'Pande Putu Sunaryang Yogam Prasanta',
+        d: 'Pande Putu Sunaryang Yogaam Prashanta',
+    }, {
+        question: 'Tanggal Lahir Pacar',
+        a: '27',
+        b: '28',
+        c: '29',
+        d: '30',
+        correct: 'b'
+    }, {
+        question: 'Tempat Lahir Pacar',
+        a: 'Denpasar',
+        b: 'Bangli',
+        c: 'Jimbaran',
+        d: 'Kintamani',
+    }
+]
